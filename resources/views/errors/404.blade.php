5e047bff0ee6c8c79aa5b9667f886e884db1c954
dafahfabiansyah <davarobiansyah@gmail.com>
2024-10-10 23:17:38 +0700

    feat : add error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{asset('css/output.css')}}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" /> --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+</head>
+
+<!-- component -->
+<main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+	<h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+	<div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+		<h2>{{ $exception->getMessage() }}</h2>
+	</div>
+	<button class="mt-5">
+      <a
+        class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+      >
+        <span
+          class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+        ></span>
+
+        <span class="relative block">
+            <a href="{{ route('login') }}" class="px-8 py-3 bg-[#1A2238] border border-current text-white rounded hover:bg-[#2B3347] transition">
+                Login
+            </a>
+        </span>
+      </a>
+    </button>
+</main>
+
+</html>
