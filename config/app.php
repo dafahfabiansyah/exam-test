f2323024eb21ef6cc0d86fcdc1c1989e6abb8413
dafahfabiansyah <davarobiansyah@gmail.com>
2024-10-11 21:22:31 +0700

    revert: reverts commit <5e047bf>

--- config/app.php
+++ config/app.php
@@ -123,16 +123,4 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Package Manifest Cache Path
-    |--------------------------------------------------------------------------
-    |
-    | Di sini kita mengarahkan Laravel untuk menggunakan path yang dapat ditulis
-    | di lingkungan read-only seperti Vercel. Kita set path cache package
-    | manifest ke /tmp.
-    |
-    */
-    'manifest' => env('APP_MANIFEST', storage_path('framework/cache')),
-    
 ];
